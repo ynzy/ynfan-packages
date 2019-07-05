af7140afd50f75b54fed5c3e52ec2aa098566cc9
zhangyong <zhangyong@163.com>
2019-07-05 16:41:58 +0800

    创建自定义header组件

--- projects/yufan/src/lib/yufan.module.ts
+++ projects/yufan/src/lib/yufan.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { YufanComponent } from './yufan.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [YufanComponent],
+  declarations: [YufanComponent, HeaderComponent],
   imports: [
   ],
-  exports: [YufanComponent]
+  exports: [YufanComponent,HeaderComponent]
 })
 export class YufanModule { }
--- projects/yufan/src/public-api.ts
+++ projects/yufan/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/yufan.service';
 export * from './lib/yufan.component';
 export * from './lib/yufan.module';
+//导出自定义组件
+export * from './lib/components/header/header.component';
